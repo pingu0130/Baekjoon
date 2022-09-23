f996b11458625e112f7283e1361ecee77205fc64
pingu0130 <110139132+pingu0130@users.noreply.github.com>
2022-09-23 14:37:45 +0900

    [Silver I] Title: 안전 영역, Time: 396 ms, Memory: 31036 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,71 +1,74 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-	static int N;
-	static int[][] map;
-	static int[][] visited;
-	static int cnt;
-	static int[] dr = { 0, -1, 0, 1 };
-	static int[] dc = { -1, 0, 1, 0 };
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
-		
-		N = Integer.parseInt(br.readLine());
-		map = new int[N][N];
-		
-		
-		// 배열채우기
-		int max = 0;
-		int ans = 0;
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-				max = Math.max(max, map[i][j]);
+	static int[][] arr, land;
+	static int N, high, max, cnt;
+	static int[][] dx = { { -1, 0 }, { 0, -1 }, { 0, 1 }, { 1, 0 } };
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		sc.nextLine();
+		arr = new int[N + 2][N + 2];
+		high = 0;
+		for (int i = 1; i < N + 1; i++) {
+			for (int j = 1; j < N + 1; j++) {
+				arr[i][j] = sc.nextInt();
+				// 물의 수위를 알아뵈기위해 가장 높은땅을 찾는다.
+				high = Math.max(arr[i][j], high);
 			}
 		}
-		//최대 높이 -1한 값부터 돌려가면서 섬이 몇개씩 나오는지 확인해보기
-		for (int d = max-1; d>=0; d--) {
-			cnt = 0;
-			
-			visited = new int[N][N];
-			
-			//큰 높이부터 돌아가면서 안전지대 세보기
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (map[i][j] > d && visited[i][j] == 0) {
-					cnt++;
-//					System.out.println(d + " " + i + " " + j +" " + map[i][j]);
-					dfs(i, j, d);
-				}
-			}
-		}
-		ans = Math.max(cnt, ans);
-		}
-		bw.write(ans+"\n");
-		bw.flush();
-		br.close();
-		bw.close();
+		// 비가 0부터 온다
+		max = 0;
+		rain(0);
+		System.out.println(max);
+		sc.close();
 	}
-	static void dfs(int r, int c, int deep) {
-		visited[r][c] = 1;
-		
-		for (int i = 0; i < 4; i++) {
-			int nr = r+dr[i];
-			int nc = c+dc[i];
-			
-			if (nr>=0 && nr<N && nc>=0 && nc<N) {
-				if(map[nr][nc] > deep && visited[nr][nc] == 0) {
-					dfs(nr, nc, deep);
+
+	// 비오면 땅찾는 함수
+	public static void rain(int r) {
+		// 비가 high만큼 오면 다 잠기니까
+		if (r == high) {
+			return;
+		}
+		// 비온다
+		water(r);
+
+		// 땅 찾기
+		for (int i = 1; i < N + 1; i++) {
+			for (int j = 1; j < N + 1; j++) {
+				if (land[i][j] != 0) {
+					cnt++;
+					land(i, j);
 				}
+			}
+		}
+		// 최대땅 저장
+		max = Math.max(cnt, max);
+		cnt = 0;
+		rain(r + 1);
+	}
+
+	// 침수중..
+	public static void water(int w) {
+		land = new int[N + 2][N + 2];
+		for (int i = 1; i < N + 1; i++) {
+			for (int j = 1; j < N + 1; j++) {
+				// 수위만큼 빼준다.
+				land[i][j] = arr[i][j] - w;
+				if (land[i][j] < 0) {
+					land[i][j] = 0;
+				}
+			}
+		}
+	}
+
+	// 인접한 땅 검사하기
+	public static void land(int x, int y) {
+		land[x][y] = 0;
+		for (int i = 0; i < 4; i++) {
+			if (land[x + dx[i][0]][y + dx[i][1]] != 0) {
+				land(x + dx[i][0], y + dx[i][1]);
 			}
 		}
 	}
