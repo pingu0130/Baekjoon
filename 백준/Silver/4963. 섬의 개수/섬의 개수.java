1e301a791d3d5654c36aebc5f95d4d6f759f4802
pingu0130 <110139132+pingu0130@users.noreply.github.com>
2022-09-23 11:27:37 +0900

    [Silver II] Title: 섬의 개수, Time: 116 ms, Memory: 13452 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -1,60 +1,70 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int[][] arr;
-	static int h, w, ans;
-	static int[][] dx = { { -1, -1 }, { -1, 0 }, { -1, 1 }, { 0, -1 }, { 0, 1 }, { 1, -1 }, { 1, 0 }, { 1, 1 } };
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
 
-	public static void landing(int x, int y) {
-		// 땅이면 0으로 바꾸기위한 함수
+		while (true) {
+			st = new StringTokenizer(br.readLine());
+			c = Integer.parseInt(st.nextToken());
+			r = Integer.parseInt(st.nextToken());
+
+			if (r == 0 && c == 0)
+				break;
+
+			arr = new int[r][c];
+
+			// 배열채우기
+			for (int i = 0; i < r; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < c; j++) {
+					arr[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+
+			int cnt = 0;
+			for (int i = 0; i < r; i++) {
+				for (int j = 0; j < c; j++) {
+					if (arr[i][j] == 1) {
+						cnt++;
+						// 연결된 육지를 삭제
+						dfs(i, j);
+					}
+				}
+			}
+			bw.write(cnt + "\n");
+		}
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+
+	static int r, c;
+	static int[][] arr;
+
+	static void dfs(int x, int y) {
+		// 사방탐색해면서 그게 일이면 0으로 바꿔줌
 		arr[x][y] = 0;
 		for (int i = 0; i < 8; i++) {
-			if (arr[x + dx[i][0]][y + dx[i][1]] == 1) {
-				// 인접한 1을 모두 0으로 바꿔준다.
-				landing(x + dx[i][0], y + dx[i][1]);
-			}
-		}
-	}
-
-	public static void island(int x, int y) {
-		// 땅이면 건너갈 수 있는지 검사
-		if (x >= h + 1 || y >= w + 1) {
-			return;
-		}
-		for (int i = 0; i < h + 1; i++) {
-			for (int j = 0; j < w + 1; j++) {
-				if (arr[i][j] == 1) {
-					ans++;
-					// 0바꾸기
-					landing(i, j);
+			int nr = x + dr[i];
+			int nc = y + dc[i];
+			// 범위 한정
+			if (nr >= 0 && nr < r && nc >= 0 && nc < c) {
+				if (arr[nr][nc] == 1) {
+					dfs(nr, nc);
 				}
 			}
 		}
-		if (y == w) {
-			island(x + 1, 1);
-		} else {
-			island(x, y + 1);
-		}
-
 	}
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		while (true) {
-			w = sc.nextInt();
-			h = sc.nextInt();
-			if (w == 0 && h == 0)
-				break;
-			arr = new int[h + 2][w + 2];
-			for (int i = 1; i < h + 1; i++) {
-				for (int j = 1; j < w + 1; j++) {
-					arr[i][j] = sc.nextInt();
-				}
-			}
-			island(1, 1);
-			System.out.println(ans);
-			ans = 0;
-		}
-		sc.close();
-	}
+	static int[] dr = { 0, -1, -1, -1, 0, 1, 1, 1 };
+	static int[] dc = { -1, -1, 0, 1, 1, 1, 0, -1 };
+
 }
