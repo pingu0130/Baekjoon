6b86ca77a700646d3a9a950c457ec3d7b3286806
pingu0130 <110139132+pingu0130@users.noreply.github.com>
2022-09-23 13:35:39 +0900

    [Silver I] Title: 단지번호붙이기, Time: 84 ms, Memory: 11724 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,73 +1,76 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int[][] arr;
-	static List<Integer> list;
 	static int N;
-	static int[][] dx = { { -1, 0 }, { 0, -1 }, { 0, 1 }, { 1, 0 } };
+	static int[][] arr;
+	static int cnt;
 
-	// 클래스를 선언해 Node에서 두개의 인자를 받을 수 있게 함
-	static class Node {
-		int r, c;
+	static int[] dr = { 0, -1, 0, 1 };
+	static int[] dc = { -1, 0, 1, 0 };
 
-		Node(int r, int c) {
-			this.r = r;
-			this.c = c;
-		}
-	}
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		sc.nextLine();
-		arr = new int[N + 2][N + 2];
-		// 정답 저장 list
-		list = new ArrayList<>();
-		for (int i = 1; i < N + 1; i++) {
-			String s = sc.nextLine();
-			int idx = 0;
-			for (int j = 1; j < N + 1; j++) {
-				arr[i][j] = s.charAt(idx) - '0';
-				idx++;
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N][N];
+
+		// 배열채우기
+		for (int i = 0; i < N; i++) {
+			String str = br.readLine();
+			for (int j = 0; j < N; j++) {
+				arr[i][j] = str.charAt(j) - '0';
 			}
 		}
-		// BFS 아파트 찾기
-		Queue<Node> queue = new LinkedList<>();
-		int cnt = 0;
-		for (int i = 1; i < N + 1; i++) {
-			for (int j = 1; j < N + 1; j++) {
-				// 아파트 발견하면 큐에 저장하고 해당 아파트 근처를 다시 수색
+
+		List<Integer> list = new ArrayList<Integer>();
+		// 순회하면서 1인 곳을 찾는다
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
 				if (arr[i][j] == 1) {
-					cnt++;
-					queue.add(new Node(i, j));
+
+					// 확인한 부분은 0으로 바꾼다.
 					arr[i][j] = 0;
-					while (!queue.isEmpty()) {
-						Node n = queue.poll();
-						// 4방탐색해서 아파트있으면 cnt
-						for (int dr = 0; dr < 4; dr++) {
-							if (arr[n.r + dx[dr][0]][n.c + dx[dr][1]] == 1) {
-								arr[n.r + dx[dr][0]][n.c + dx[dr][1]] = 0;
 
-								cnt++;
-								queue.add(new Node(n.r + dx[dr][0], n.c + dx[dr][1]));
-							}
-
-						}
-					} // while
+					cnt = 1;
+					dfs(i, j);
 					list.add(cnt);
-					cnt = 0;
 				}
 			}
-		} // for
+		}
 		Collections.sort(list);
-		System.out.println(list.size());
+		bw.write(list.size() + "\n");
 		for (int i = 0; i < list.size(); i++) {
-			System.out.println(list.get(i));
+			bw.write(list.get(i) + "\n");
+		}
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+
+	static void dfs(int r, int c) {
+		// 팔방참색을 하면서 주변에 1인 있는지 찾아본다.
+		for (int d = 0; d < 4; d++) {
+			int nr = r + dr[d];
+			int nc = c + dc[d];
+			if (nr >= 0 && nr < N && nc >= 0 && nc < N) {
+				if (arr[nr][nc] == 1) {
+					// 중복되면 안되니까 0으로 만들어주기
+					arr[nr][nc] = 0;
+					// 1이있으면 cnt++
+					cnt = cnt + 1;
+					dfs(nr, nc);
+				}
+			}
 		}
 	}
 }
