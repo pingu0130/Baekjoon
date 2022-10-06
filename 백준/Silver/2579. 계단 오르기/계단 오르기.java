a33230ee7e8cdfe894ef69e709101a9032976a17
pingu0130 <110139132+pingu0130@users.noreply.github.com>
2022-10-06 15:22:19 +0900

    [Silver III] Title: 계단 오르기, Time: 128 ms, Memory: 13240 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,29 +1,34 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N+1]; // 1이 나올 경우를 대비
+	static int N, cnt;
+	static int[] map;
+    //0과 null을 구분하기위해 integer로 받기
+	static Integer[] memo;
 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		map = new int[N + 1];
 		for (int i = 1; i <= N; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
+			map[i] = sc.nextInt();
 		}
-
-		int[] dp = new int[N+1];
-
-		dp[1] = arr[1];
-		
-		if(N>=2) dp[2] = arr[1] + arr[2];
-
-		for (int i = 3; i <= N; i++) {
-			dp[i] = Math.max(dp[i - 3] + arr[i - 1], dp[i-2]) + arr[i];
+		memo = new Integer[N + 1];
+		memo[0] = map[0];
+		memo[1] = map[1];
+		if (N >= 2) {
+			memo[2] = map[1] + map[2];
 		}
+		System.out.println(up(N));
+		sc.close();
+	}
 
-		System.out.println(dp[N]);
+	private static int up(int n) {
+		if (memo[n] == null) {
+			// 점프 후 걷기, 혹은 걷기 후 점프 두가지 방법이 있기 때문에
+			memo[n] = Math.max(up(n - 2), up(n - 3) + map[n - 1]) + map[n];
+		}
+		return memo[n];
 	}
 }
