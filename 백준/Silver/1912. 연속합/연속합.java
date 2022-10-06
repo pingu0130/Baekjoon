a3b370d2b16e96e4811b4cdb5af21c628bb1f211
pingu0130 <110139132+pingu0130@users.noreply.github.com>
2022-10-06 15:49:20 +0900

    [Silver II] Title: 연속합, Time: 676 ms, Memory: 90408 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -1,25 +1,25 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-	 public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	
-		int N = Integer.parseInt(br.readLine());
-		int[] dp = new int[N];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		// 첫번째 애는 비교할 대상이 없으니까 채워주고 시작
-		dp[0] = Integer.parseInt(st.nextToken());
-		int max = dp[0]; // 최대로 큰 값을 저장해줄거다
-         
-		for(int i = 1; i < N; i++) {
-			int tmp = Integer.parseInt(st.nextToken());
-			dp[i] = Math.max(dp[i-1]+tmp, tmp);
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] arr = new int[N + 1];
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		int[] dp = new int[N + 1];
+		dp[0] = arr[0];
+		int max = arr[0];
+		for (int i = 1; i < N; i++) {
+			//
+			dp[i] = Math.max(dp[i - 1] + arr[i], arr[i]);
+			// 최댓값갱신
 			max = Math.max(max, dp[i]);
 		}
 		System.out.println(max);
-	 }
+		sc.close();
+	}
+
 }
