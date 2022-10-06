a516d0a1973848437d331a64d97f8fcdf1690658
pingu0130 <110139132+pingu0130@users.noreply.github.com>
2022-10-06 13:38:26 +0900

    [Silver III] Title: 피보나치 함수, Time: 124 ms, Memory: 13128 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -1,39 +1,41 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int N;
+	static int[] memo;
+	static int[][] cnt;
 
-		int T = Integer.parseInt(br.readLine());
-
-		for (int t = 1; t <= T; t++) {
-
-			int N = Integer.parseInt(br.readLine());
-			int[][] dp = new int[N + 1][2];
-
-			for (int[] tmp : dp) {
-				Arrays.fill(tmp, -1);
-			}
-
-			dp[0][0] = 1; // 0호출 수
-			dp[0][1] = 0; // 0호출 수
-
-			if (N >= 1) {
-				dp[1][0] = 0; // 1호출 수
-				dp[1][1] = 1; // 1호출 수
-			}
-
-			if (N >= 2) {
-				for (int i = 2; i <= N; i++) {
-					dp[i][0] = dp[i-1][0]+dp[i-2][0];
-					dp[i][1] = dp[i-1][1]+dp[i-2][1];
-				}
-			}
-
-			System.out.println(dp[N][0] + " " + dp[N][1]);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int TC = sc.nextInt();
+		for (int tc = 0; tc < TC; tc++) {
+			N = sc.nextInt();
+			memo = new int[41];
+			cnt = new int[41][2];
+			Arrays.fill(memo, -1);
+			memo[0] = 0;
+			memo[1] = 1;
+			cnt[0][0] = 1;
+			cnt[1][1] = 1;
+			fibo(N);
+			System.out.println(cnt[N][0] + " " + cnt[N][1]);
 		}
+		sc.close();
+	}
+
+	private static int fibo(int n) {
+		if (n == 0) {
+			return 0;
+		}
+		if (n == 1) {
+			return 1;
+		}
+		if (memo[n] == -1) {
+			memo[n] = fibo(n - 1) + fibo(n - 2);
+			cnt[n][0] = cnt[n - 1][0] + cnt[n - 2][0];
+			cnt[n][1] = cnt[n - 1][1] + cnt[n - 2][1];
+		}
+		return memo[n];
 	}
 }
