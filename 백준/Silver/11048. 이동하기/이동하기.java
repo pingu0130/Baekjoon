dba793d16f41af87796703bfcfd49674b14bc934
pingu0130 <110139132+pingu0130@users.noreply.github.com>
2022-10-06 14:20:47 +0900

    [Silver II] Title: 이동하기, Time: 1292 ms, Memory: 309592 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+
+		int[][] map = new int[N + 1][M + 1];
+		for (int i = 1; i < N + 1; i++) {
+			for (int j = 1; j < M + 1; j++) {
+				map[i][j] = sc.nextInt();
+			}
+		}
+		for (int i = 1; i < N + 1; i++) {
+			for (int j = 1; j < M + 1; j++) {
+				map[i][j] += Math.max(map[i][j - 1], Math.max(map[i - 1][j], map[i - 1][j - 1]));
+			}
+		}
+		System.out.println(map[N][M]);
+	}
+}
